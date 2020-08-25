adcbb24080bfdac9758bd2114b57b3b32c523be4
Rayo Verweij <rayo.verweij@live.nl>
2020-08-25 18:53:45 +0200

    Updates

---
+++ precache-manifest.4fd5ee27e693878eb0fcc360192e0812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ccb63e8ba2472f39ad5738cd70f115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57f0e1c92a2ec89d9059",
+    "url": "/static/css/main.dacf4415.chunk.css"
+  },
+  {
+    "revision": "e84dd5cbb0d30dc805cb",
+    "url": "/static/js/2.1c70c0e6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1c70c0e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f0e1c92a2ec89d9059",
+    "url": "/static/js/main.5ebb0e98.chunk.js"
+  },
+  {
+    "revision": "7db252e00118f5296f23",
+    "url": "/static/js/runtime-main.0c5be807.js"
+  }
+]);
